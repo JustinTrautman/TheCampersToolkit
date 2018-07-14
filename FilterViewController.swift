34f299fbd88912a71e4578a2b167cdcecd2b06b0
Justin Trautman <justin@modularmobile.net>
2018-07-13 21:31:20 -0600

    Adds filter viewController

---
+++ FilterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FilterViewController.swift
+//  TheCampersToolkit
+//
+//  Created by Justin Trautman on 7/13/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import Foundation
