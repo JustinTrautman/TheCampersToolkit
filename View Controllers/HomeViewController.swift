00440187a06caa275adc48968ee55252900f350c
Justin Trautman <justin@modularmobile.net>
2018-07-13 18:40:06 -0600

    Initial Project

---
+++ View Controllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  TheCampersToolkit
+//
+//  Created by Justin Trautman on 7/13/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import Foundation
